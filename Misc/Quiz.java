0853edc0fffb4579f8198e1f7ebd345f20073a4d
Chay Hui Xiang <chayhuixiang@gmail.com>
2022-08-09 01:00:07 +0800

    Create Quiz.java

---
+++ Misc/Quiz.java
@@ -0,0 +1,18 @@
+package Misc;
+
+class Quiz {
+  public static void main(String[] args)
+
+  {
+  
+    String word = "abcde";
+
+    for (int i = 0; i <4; i+=2) {
+
+      System.out.println(i);
+      System.out.print(word.charAt(i));
+    }
+    
+  
+  }
+}
